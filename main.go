4e331666c5e60ca0de71521eafe0151532061d32
ProgressiveRabbit <databuilders1024@gmail.com>
2021-11-06 22:19:50 +0800

    recipe data model

--- main.go
+++ main.go
@@ -2,7 +2,16 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+
 func main()  {
 	router := gin.Default()
 	router.Run()
+}
+
+type Recipe struct {
+	Name string `json:"Name"`
+	Tag []string `json:"tags"`
+	Ingredients []string `json:"ingredients"`
+	Instruction []string `json:"instruction"`
+	PublishedAt []string `json:"published_at"`
 }
